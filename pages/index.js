e1e129f7490fab70a2688fe3cb85e8fc68bb6ef3
Ahmed Sami <ahmedsami03@protonmail.com>
2022-08-13 08:26:14 +0200

    .

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import styles from "../styles/Home.module.css";
 import Card from "../components/Card";
+import {useAuth} from "../contexts/AuthContext";
+import Link from "next/link";
 
 export default function Home() {
+    const {currentUser} = useAuth();
+
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to the <a href="#">CMS!</a>
+                    Welcome to the {" "}
+                    <Link href={currentUser ? "/dashboard" : "/login"}>
+                        <a>CMS!</a>
+                    </Link>
                 </h1>
                 <div className={styles.grid}>
-                    <Card width={"360px"} href={"/login"} title={"Login"}
-                          text={"Instantly deploy your Next.js site to a public URL with Vercel."}/>
-                    <Card width={"360px"} href={"/dashboard"} title={"Dashboard"}
-                          text={"Instantly deploy your Next.js site to a public URL with Vercel."}/>
+                    {currentUser &&
+                        <Card width={"360px"} href={"/dashboard"} title={"Dashboard"}
+                              text={"Visit the dashboard from where you can write new posts."}/>
+                    }
+                    {!currentUser &&
+                        <Card width={"360px"} href={"/login"} title={"Login"}
+                              text={"Login to your account in order to access the dashboard."}/>
+                    }
                 </div>
             </main>
         </div>
