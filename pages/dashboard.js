d17a3f308db52d890585811c0bd5834f266b2851
Ahmed Sami <ahmedsami03@protonmail.com>
2022-08-14 09:22:16 +0200

    .

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,7 +7,9 @@ import Link from "next/link";
 export default function Dashboard() {
     const {currentUser} = useAuth();
     const [alert, setAlert] = useState({msg: "", variant: "default"});
+    const [socketMsg, setSocketMsg] = useState({msg: "", variant: "default"});
     const [loading, setLoading] = useState(false);
+    const [wsInstance, setWsInstance] = useState(null);
 
     const saveToCache = (id, val) => {
         if (val === "") {
@@ -25,9 +27,66 @@ export default function Dashboard() {
         }
     };
 
+    function connectToWs() {
+        const ws = new WebSocket("ws://" + document.location.hostname + ":9100/socket");
+        ws.addEventListener("open", () => {
+            setAlert({msg: "Connected to socket!", variant: "success"});
+            setWsInstance(ws);
+        })
+        ws.addEventListener("error", () => {
+            setAlert({msg: "Failed to connect to socket!", variant: "danger"});
+            setWsInstance(null);
+        })
+        ws.addEventListener("close", (evt) => {
+            switch (evt.code) {
+                case 1000:
+                    setAlert({msg: "Connection closed by client!", variant: "warning"});
+                    break;
+                case 1006:
+                    setAlert({msg: "Connection closed by server!", variant: "danger"});
+                    break;
+            }
+            setWsInstance(null);
+        })
+        ws.addEventListener("message", (evt) => {
+            setSocketMsg({msg: evt.data, variant: "primary"});
+        })
+    }
+
+    function handleSubmit() {
+        if (wsInstance !== null) {
+            try {
+                setLoading(true);
+                setAlert({msg: "", variant: "default"});
+                const msg = {
+                    "article_path": document.getElementById("article_path").value,
+                    "article_content": document.getElementById("article_content").value,
+                };
+                wsInstance.send(JSON.stringify(msg));
+                setAlert({msg: "Sent to socket!", variant: "success"});
+            } catch {
+                setAlert({msg: "Failed to send to socket!", variant: "danger"});
+            } finally {
+                setLoading(false);
+            }
+        }
+    }
+
+    function handleConnect() {
+        if (window["WebSocket"] && wsInstance === null) {
+            connectToWs()
+        }
+    }
+
+    function handleClose() {
+        if (wsInstance !== null) {
+            wsInstance.close(1000);
+        }
+    }
+
     useEffect(() => {
         document.getElementById("article_path").value = getFromCache("article_path");
-        document.getElementById("article_txt").value = getFromCache("article_txt");
+        document.getElementById("article_content").value = getFromCache("article_content");
     }, []);
 
     return (
@@ -45,7 +104,9 @@ export default function Dashboard() {
                 <div className={styles.form} style={{maxWidth: "900px"}}>
                     <div className={styles.formHeader}>New Post</div>
                     <div className={styles.formBody}>
-                        {alert.msg && <Alert msg={alert.msg} variant={alert.variant}/>}
+                        {alert.msg &&
+                            <Alert variant={alert.variant}>{alert.msg}</Alert>
+                        }
                         <form onSubmit={e => e.preventDefault()}>
                             <div>
                                 <label className={styles.formLabel}>Path</label>
@@ -55,11 +116,30 @@ export default function Dashboard() {
                             </div>
                             <div>
                                 <label className={styles.formLabel}>Article</label>
-                                <textarea className={styles.formTextarea} id="article_txt"
+                                <textarea className={styles.formTextarea} id="article_content"
                                           placeholder="Article" required="" rows={10}
-                                          onChange={(e) => saveToCache("article_txt", e.target.value)}/>
+                                          onChange={(e) => saveToCache("article_content", e.target.value)}/>
                             </div>
-                            <button className={styles.formButton} disabled={loading} type="submit">Refresh</button>
+                            {wsInstance !== null && <>
+                                {socketMsg.msg && <>
+                                        <label className={styles.formLabel}>Preview URL:</label>
+                                        <Alert variant={socketMsg.variant}>{socketMsg.msg}</Alert>
+                                </>}
+                            </>}
+                            {wsInstance !== null &&
+                                <button className={styles.formButton} disabled={loading}
+                                        onClick={handleSubmit}>Send</button>
+                            }
+                            {wsInstance !== null &&
+                                <button className={styles.formButton} disabled={loading}
+                                        onClick={handleClose}>Close connection</button>
+                            }
+                            {wsInstance === null && <>
+                                {currentUser &&
+                                    <button className={styles.formButton} disabled={loading}
+                                            onClick={handleConnect}>Connect to socket</button>
+                                }
+                            </>}
                         </form>
                     </div>
                 </div>
